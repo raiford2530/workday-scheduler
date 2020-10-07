5591fb3fa38179396311908fdf5b0c78d6542b75
Rashawn Raiford <raiford87@gmail.com>
2020-10-07 03:18:38 -0400

    Add code to color timeblocks to indicate if they are in the past, present, or future

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     var currentDay = $('#currentDay');
-    //Get current day and time using moment
+    //Create and store moment to get current day and time
     var currentMoment = moment();
 
     //Set text to show current day and date in the #currentDay paragraph
@@ -18,11 +18,35 @@ $(document).ready(function () {
         //Create time blocks
         for (var i = 0; i < hours.length; i++) {
 
-          var row = $('<div class="row no-gutters">');
+          var row = $('<div class="row no-gutters time-block">');
           row.append('<div class="col-2 col-sm-2 col-lg-1"><div class="hour">' + hours[i].format("ha") + '</div></div>');
-          row.append('<div class="col-9 col-sm-9 col-lg-10"><textarea class="form-control"></textarea></div>');
+          row.append('<div class="col-9 col-sm-9 col-lg-10"><textarea class="form-control description" data-hour="' + hours[i].format("H") + '"></textarea></div>');
           row.append('<div class="col-1 col-sm-1 col-lg-1"><button class="saveBtn"><i class="far fa-save"></i></button></div>');
           $('.container').append(row);
+        }
+
+        //Get the current hour in 24 hour format
+        var currentHour = parseInt(currentMoment.format("H"));
+        //Get the textareas in each timeblock
+        var eventList = $('.row textarea');
+        console.log(eventList);
+        for (var i = 0; i < eventList.length; i++) {
+
+          var textArea = $(eventList[i]);
+          //Get the hour that the time block corresponds to from the data-hour attribute
+          var hour = parseInt(textArea.attr('data-hour'));
+
+          //If the time block hour is later than the current hour then add the future class
+          //else if the time block hour is earlier than the current time then add the past class
+          //else add the present class
+          if (hour > currentHour) {
+            textArea.addClass("future");
+          } else if (hour < currentHour) {
+            textArea.addClass("past");
+          }
+          else {
+            textArea.addClass("present");
+          }
         }
       }
 
